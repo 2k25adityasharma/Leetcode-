9a3d6a45f5e50ad75c157a0995b09688dedcb66c
2k25adityasharma <2k25aiml2513475@gmail.com>
2026-06-26 23:00:10 +0530

    Time: 7 ms (20.86%), Space: 17.3 MB (82.85%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        vector<string>v;
-        vector<string>ans;
-    int n = words.size();
-    unordered_map<string,int>mp;
-        for(int i =0;i<n;i++){
-      mp[words[i]]++;
-    }
-    for(auto i:mp)
-        v.push_back(i.first);
-   int c =0;
-   while(c<k){
-       int j=-1;
-    int f=INT_MIN;
-
-    for(int i =0;i<v.size();i++){
-        if(f<mp[v[i]]){
-       f= mp[v[i]];
-       j=i;
+        vector<string> v;
+        vector<string> ans;
+        int n = words.size();
+        unordered_map<string, int> mp;
+        
+        for(int i = 0; i < n; i++){
+            mp[words[i]]++;
         }
-        else if(mp[v[i]] == f && v[i] < v[j])
-        {
-            j = i;
+        
+        for(auto i : mp)
+            v.push_back(i.first);
+
+        
+        sort(v.begin(), v.end());
+
+        int c = 0;
+        while(c < k){
+            int j = 0; 
+            int f = mp[v[0]];
+
+          
+            for(int i = 1; i < v.size(); i++){
+                if(mp[v[i]] > f){ 
+                    f = mp[v[i]];
+                    j = i;
+                }
+            }
+            
+            ans.push_back(v[j]);
+            v.erase(v.begin() + j); 
+            c++;
         }
-    }
-    ans.push_back(v[j]);
-    v.erase(v.begin()+j);
-  c++;
-   }
 
-
- 
-
-   return ans;
+        return ans;
     }
 };
