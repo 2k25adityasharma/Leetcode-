9f106564e026578e2112cd86a21d4cc4a02e262c
2k25adityasharma <2k25aiml2513475@gmail.com>
2026-07-03 23:04:19 +0530

    Time: 0 ms (100%), Space: 7.8 MB (56.27%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool checkPerfectNumber(int n) {
        int sum =0;
+       if(n==1) return false;
         for(int i =1;i*i<=n;i++){
            if(n%i==0){
-            if(n!=i)
+            
             sum +=i;
             int a = n/i;
             if(i!=a && a !=n){
