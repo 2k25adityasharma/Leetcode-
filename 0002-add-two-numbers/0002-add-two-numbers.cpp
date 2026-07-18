fa86d4b1971d1d0e831a285de5a8ce38638bf166
2k25adityasharma <2k25aiml2513475@gmail.com>
2026-07-18 21:56:20 +0530

    Time: 3 ms (34.36%), Space: 77.2 MB (45.85%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,84 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * }; 
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode* m=l1;
+    ListNode* n=l2;
+    ListNode* sum =new ListNode (0);
+    ListNode*temp=sum;
+    int c =0;
+ ////////////////////////////////////////////both same loop
+    while(m!=NULL && n != NULL){
+        int a = c+m->val+n->val;
+        if(a>=10){
+          ListNode * t= new ListNode (a%10);
+          c=a/10;
+     sum->next= t;
+     sum=sum->next;
+
+        }
+        else{
+     ListNode * t= new ListNode (a);
+     c=0;
+     sum->next= t;
+     sum=sum->next;
+
+    } m= m->next; 
+     n= n->next;
+    }
+///////////////////////////////////////////////////////////
+    while(m!=NULL){
+        int a = c+m->val;
+        if(a>=10){
+      ListNode * t= new ListNode (a%10);
+      c=a/10;
+     sum->next= t;
+     sum=sum->next;
+        }
+        else{
+          ListNode * t= new ListNode (a);
+     c=0;
+     sum->next= t;
+     sum=sum->next;
+        }
+        m=m->next;
+    }
+////////////////////////////////////////
+ while(n!=NULL){
+        int a = c+n->val;
+        if(a>=10){
+      ListNode * t= new ListNode (a%10);
+      c=a/10;
+     sum->next= t;
+     sum=sum->next;
+        }
+        else{
+          ListNode * t= new ListNode (a);
+     c=0;
+     sum->next= t;
+     sum=sum->next;
+        }
+        n=n->next;
+    }
+
+
+    if(c){
+        ListNode * t= new ListNode (c);
+        sum->next=t;
+        sum=sum->next;
+    }
+    ListNode *tempa=temp;
+    temp= temp->next;
+    delete tempa;
+     return temp;
+    }
+};
