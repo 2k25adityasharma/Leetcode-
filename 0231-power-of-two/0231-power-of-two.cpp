59bfa39e8e7f4440ee4733914192949d9ddd4d32
2k25adityasharma <2k25aiml2513475@gmail.com>
2026-08-13 12:08:42 +0530

    Time: 3 ms (3.36%), Space: 7.8 MB (50.48%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1)
+        return true;
+        double a = n;
+        while(a>0){
+            if(a==1)
+            return true;
+         a/=2;
+         
+        }
+        if(a==(int)a && a==1)
+        return true;
+        else 
+        return false;
+    }
+};
