4f7226bae1d81e21f96928a54023e04f0ecbae52
Aditya Sharma <2k25aiml2513475@gmail.com>
2026-09-26 21:54:39 +0530

    Time: 4 ms (8.54%), Space: 14.4 MB (14.08%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& token, int power) {
+        int n= token.size();
+        int i =0;
+        int j = n-1;
+        sort(token.begin(),token.end());
+        int ans =0;
+        while(i<j){
+            if(power>=token[i]){
+                power-=token[i];
+                ans++;
+                i++;
+            }
+           
+            else {
+                ans--;
+                power+=token[j];
+                j--;
+                if(ans <0)
+                return 0;
+            }
+
+        }
+        if(i==j && power>=token[i]){
+            ans++;
+         }
+         
+        return ans;
+    }
+};
